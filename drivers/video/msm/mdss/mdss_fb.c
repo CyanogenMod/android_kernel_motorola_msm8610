91f672655be93719e12b3db3a5439cae9b17245f
percy-g2 <gahlotpercy@gmail.com>
2015-04-03 08:34:14 +0200

    Revert "msm: mdss: Add KCAL support for rgb/lut control (MDP3)"
    This reverts commit 98633f7655b1ae19dd4b0b90ad1d6af01c74c07b.
    
    Change-Id: Ic547df62cf24d6c6c4e8266eef39c35e7634d1ff

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2391,7 +2391,7 @@ static int mdss_fb_set_lut(struct fb_info *info, void __user *p)
 	if (ret)
 		return ret;
 
-	mfd->mdp.lut_update(mfd, &cmap, NULL);
+	mfd->mdp.lut_update(mfd, &cmap);
 	return 0;
 }
 
